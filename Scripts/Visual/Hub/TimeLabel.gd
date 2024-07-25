0a5f8a7774587d744f8bc8cab96550b33bf95922
StarrySeaDev <mryangjh@yeah.net>
2024-07-25 14:06:07 +0800

    完成大厅UI

---
+++ Scripts/Visual/Hub/TimeLabel.gd
@@ -0,0 +1,14 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# var time_dict = Time.get_time_dict_from_system()
+	# var time = time_dict["hour"] as String + " : " + time_dict["minute"] as String + " : " + time_dict["second"] as String
+	# text = time
+	pass
--- Scripts/Visual/Hub/hub_scene.gd
+++ Scripts/Visual/Hub/hub_scene.gd
@@ -1,15 +1,16 @@
 extends Control
 
-@onready var timeLabel = $AppTile/MarginContainer/HBoxContainer/TimeLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_get_current_time()
-	queue_redraw()
+	pass
+
+
 
 func _on_start_btn_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Visual/select_scene.tscn")
@@ -21,7 +22,3 @@ func _on_res_mgr_btn_pressed():
 
 func _on_settings_btn_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Visual/Settings/settings_scene.tscn")
-
-func _get_current_time():
-	var time = Time.get_datetime_dict_from_system()
-	timeLabel.text = time["hour"] as String + ":" + time["minute"] as String + ":" + time["second"] as String
